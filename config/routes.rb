1d81f025dcd8c56b849675887609cdb7f5919a7f
Kayt <kayt.campbell@ama.ab.ca>
2012-08-24 16:59:57 -0600

    added comments functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 MySite::Application.routes.draw do
-  resources :posts
 
-  root :to => 'posts#index'
+resources :posts do
+  resources :comments
+end
+  
+root :to => 'posts#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
