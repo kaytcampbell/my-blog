0f9b7abc8682aa72ba66d0a39da9f53d8b7dc4c7
Kayt <kayt.campbell@ama.ab.ca>
2012-12-21 15:28:27 -0700

    added roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Role.create_or_find_by_name("Admin")
+Role.create_or_find_by_name("Moderator")
+Role.create_or_find_by_name("Author")
